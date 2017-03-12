ed478bb36b29077498f7ab270ff03f1aa2d2e753
Jean-Francois Chenier <j.f_chenier@hotmail.com>
2017-03-11 22:50:20 -0500

    state machine with switch case

--- Project/Tasks/state_machine_task.c
+++ Project/Tasks/state_machine_task.c
@@ -53,6 +53,10 @@
 #include "memstruct.h"
 #include "can_rx_task.h"
 
+enum {
+	STATE_1 = 1, STATE_2, STATE_3, STATE_4, STATE_5
+};
+
 /*
  *********************************************************************************************************
  *                                            LOCAL DATA
@@ -61,17 +65,13 @@
 static OS_TCB stateMachineTaskTCB;
 static CPU_STK stateMachineTaskStk[APP_CFG_STATE_MACHINE_TASK_STK_SIZE];
 
-struct state
-{
-    void (*next)(struct state *state);
-    void *arg;
-};
+static uint16_t next_state = 1;
 
-void state_1(struct state *state);
-void state_2(struct state *state);
-void state_3(struct state *state);
-void state_4(struct state *state);
-void state_5(struct state *state);
+void state_1(void);
+void state_2(void);
+void state_3(void);
+void state_4(void);
+void state_5(void);
 
 /*
  *********************************************************************************************************
@@ -90,78 +90,95 @@ void state_5(struct state *state);
  * Return(s)   : none.
  *********************************************************************************************************
  */
-static void state_machine_task(void *p_arg)
-{
-    struct state state = { state_1, 0 };
+static void state_machine_task(void *p_arg) {
+	while (1) {
+		switch (next_state) {
 
-    while(state.next){
-        state.next(&state);
-    }
+		case STATE_1:
+			state_1();
+			break;
+
+		case STATE_2:
+			state_2();
+			break;
+
+		case STATE_3:
+			state_3();
+			break;
+
+		case STATE_4:
+			state_4();
+			break;
+
+		case STATE_5:
+			state_5();
+			break;
+
+		default:
+			break;
+			state_2();
+		}
+	}
 }
 
-void state_1(struct state *state)
-{
-    OS_ERR err;
+void state_1(void) {
+	OS_ERR err;
 
-    printf("this is the state 1\n\r");
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+	printf("this is the state 1\n\r");
+	OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
-    if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
-    	state->next = state_5;
-    else
-    	state->next = state_2;
+	if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
+		next_state = STATE_5;
+	else
+		next_state = STATE_2;
 }
 
-void state_2(struct state *state)
-{
-    OS_ERR err;
+void state_2(void) {
+	OS_ERR err;
 
-    printf("this is the state 2\n\r");
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+	printf("this is the state 2\n\r");
+	OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
-    if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
-    	state->next = state_1;
-    else
-    	state->next = state_3;
+	if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
+		next_state = STATE_1;
+	else
+		next_state = STATE_3;
 }
 
-void state_3(struct state *state)
-{
-    OS_ERR err;
+void state_3(void) {
+	OS_ERR err;
 
-    printf("this is the state 3\n\r");
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+	printf("this is the state 3\n\r");
+	OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
-    if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
-    	state->next = state_2;
-    else
-    	state->next = state_4;
+	if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
+		next_state = STATE_2;
+	else
+		next_state = STATE_4;
 }
 
-void state_4(struct state *state)
-{
-    OS_ERR err;
+void state_4(void) {
+	OS_ERR err;
 
-    printf("this is the state 4\n\r");
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+	printf("this is the state 4\n\r");
+	OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
-    if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
-    	state->next = state_3;
-    else
-    	state->next = state_5;
+	if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
+		next_state = STATE_3;
+	else
+		next_state = STATE_5;
 }
 
-void state_5(struct state *state)
-{
-    OS_ERR err;
+void state_5(void) {
+	OS_ERR err;
 
-    printf("this is the state 5\n\r");
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+	printf("this is the state 5\n\r");
+	OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
-    if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
-    	state->next = state_4;
-    else
-    	state->next = state_1;
+	if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
+		next_state = STATE_4;
+	else
+		next_state = STATE_1;
 }
 
 /*
@@ -181,21 +198,16 @@ void state_5(struct state *state)
  * Return(s)   : none.
  *********************************************************************************************************
  */
-void state_machine_task_create(void)
-{
-    OS_ERR err;
+void state_machine_task_create(void) {
+	OS_ERR err;
 
-    OSTaskCreate((OS_TCB *)&stateMachineTaskTCB,
-            (CPU_CHAR *)"state machine task",
-            (OS_TASK_PTR)state_machine_task,
-            (void *)0,
-            (OS_PRIO)APP_CFG_STATE_MACHINE_TASK_PRIO,
-            (CPU_STK *)&stateMachineTaskStk[0],
-            (CPU_STK_SIZE)stateMachineTaskStk[APP_CFG_STATE_MACHINE_TASK_STK_SIZE / 10],
-            (CPU_STK_SIZE)APP_CFG_STATE_MACHINE_TASK_STK_SIZE,
-            (OS_MSG_QTY)0,
-            (OS_TICK)0,
-            (void *)0,
-            (OS_OPT)(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR ),
-            (OS_ERR *)&err);
+	OSTaskCreate((OS_TCB *) &stateMachineTaskTCB,
+			(CPU_CHAR *) "state machine task", (OS_TASK_PTR) state_machine_task,
+			(void *) 0, (OS_PRIO) APP_CFG_STATE_MACHINE_TASK_PRIO,
+			(CPU_STK *) &stateMachineTaskStk[0],
+			(CPU_STK_SIZE) stateMachineTaskStk[APP_CFG_STATE_MACHINE_TASK_STK_SIZE
+					/ 10], (CPU_STK_SIZE) APP_CFG_STATE_MACHINE_TASK_STK_SIZE,
+			(OS_MSG_QTY) 0, (OS_TICK) 0, (void *) 0,
+			(OS_OPT) (OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR ),
+			(OS_ERR *) &err);
 }
